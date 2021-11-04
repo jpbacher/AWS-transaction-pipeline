908674b432b860e40dcd7711b49ed1a58cdd1eb3
jpbacher <bacherjp@gmail.com>
2021-11-04 17:45:56 -0400

    added key parameters

--- 3.0-process/upload-to-s3.py
+++ 3.0-process/upload-to-s3.py
@@ -1,30 +1,55 @@
-from __future__ import print_function
-
-import base64
-import json
+import logging
 import boto3
+from botocore.exceptions import ClientError
+import os
+import configparser
 from datetime import datetime
 
 
-time_now = datetime.now()
-time_str = time_now.strftime('%Y/%m/%d-%H/%M/%S')
 
-s3 = boto3.client('s3')
+PROJ_ROOT = os.path.join(os.pardir)
+data_path = os.path.join(PROJ_ROOT, 'data', 'train.csv')
+aws_path = os.path.join(PROJ_ROOT, 'aws.cfg')
 
+date_now = datetime.now()
+time_stamp_str = date_now.strftime("%Y/%m/%d-%H:%M:%S")
 
-def lambda_handler(event, context):
-    trans_records = []
-    for record in event['Records']:
-        # decode kinesis data
-        decoded_record = base64.b64decode(record["kinesis"]["data"])
-        trans_records.append(decoded_record)
+def upload_file(access_key, secret_key, file_path, bucket, s3_obj_name):
+    """[summary]
+
+    Args:
+        access_key: AWS access key
+        secret_key: AWS secret access key
+        file_path (string]): file path to file on local computer
+        bucket (string): bucket name created on AWS console
+        s3_obj_name (string): name of file appearing on AWS 
+
+    Returns:
+        [type]: [description]
+    """
     
-    event_count = len(trans_records)
-    # convert list to string
-    trans_records_str = '/n'.join(map(str, trans_records))
-    # file with a timestamp
-    timestamp_file = f'{time_str}-transaction.txt'
-    # send file to s3
-    response = s3.put_object(Body=trans_records_str, Bucket='s3-transactions', Key=timestamp_file)
+    session = boto3.Session(
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key
+    )
+    s3 = session.resource('s3')
+    try: 
+        response = s3.Bucket(bucket).upload_file(file_path, s3_obj_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
+if __name__ == '__main__':
     
-    return f'Successfully placed {len(event_count)} records into s3' 
+    config = configparser.ConfigParser()
+    config.read_file(open(aws_path))
+    
+    access_key = config.get('S3', 'KEY')
+    secret_key = config.get('S3', 'SECRET')
+    
+    bucket_name = 's3cctransactions'
+    obj_name = f'transactions-{time_stamp_str}.csv'
+    
+    upload_file(access_key, secret_key, data_path, bucket_name, obj_name)
